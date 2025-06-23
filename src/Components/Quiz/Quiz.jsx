9726e350f97d1c2e094f9a5fe9cc2d0a1dc3901a
Vedant1607 <sinhavedant0705@gmail.com>
2025-06-23 11:11:49 +0530

    next function to display next questions

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -7,6 +7,8 @@ const Quiz = () => {
     let [index, setIndex] = useState(0);
     let [question, setQuestion] = useState(data[index]);
     let [lock, setLock] = useState(false);
+    let [score, setScore] = useState(0);
+
     let Option1 = useRef(null);
     let Option2 = useRef(null);
     let Option3 = useRef(null);
@@ -19,12 +21,25 @@ const Quiz = () => {
             if (question.ans === ans) {
                 e.target.classList.add("correct");
                 setLock(true);
+                setScore(prev=>prev+1);
             }
             else {
                 e.target.classList.add("wrong");
                 setLock(true);
                 options_array[question.ans-1].current.classList.add("correct");
             }
+        }
+    }
+
+    const next = () => {
+        if (lock === true){
+            setIndex(++index);
+            setQuestion(data[index]);
+            setLock(false);
+            options_array.map((option)=>{
+                option.current.classList.remove("wrong");
+                option.current.classList.remove("correct");
+            })
         }
     }
 
@@ -39,8 +54,8 @@ const Quiz = () => {
                 <li ref={Option3} onClick={(e) => {checkAnswer(e,3)}}>{question.option3}</li>
                 <li ref={Option4} onClick={(e) => {checkAnswer(e,4)}}>{question.option4}</li>
             </ul>
-            <button>Next</button>
-            <div className="index">1 of 5 questions</div>
+            <button onClick={next}>Next</button>
+            <div className="index">{index+1} of {data.length} questions</div>
         </div>
     )
 }
